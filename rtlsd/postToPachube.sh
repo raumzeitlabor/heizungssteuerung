75e08a458985d2e56a8d148006a1068810ff64a5
Severin Schols <severin@schols.de>
2011-12-17 13:02:40 +0100

    Removed API-Key

--- rtlsd/postToPachube.sh
+++ rtlsd/postToPachube.sh
@@ -3,5 +3,5 @@ stty -F /dev/ttyUSB0 cs8 9600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig
 
 while [ 1 ]
 do
-head -n 1 /dev/ttyUSB0 | xargs -I xxx sed 's/%VALUE%/xxx/g' template.json | curl --request PUT -d @- --header "X-PachubeApiKey: ov8Hbl6Ul5zdqerhn2U77WphRQ45tlwrSRi0VndUGYM" http://api.pachube.com/v2/feeds/42055
+head -n 1 /dev/ttyUSB0 | xargs -I xxx sed 's/%VALUE%/xxx/g' template.json | curl --request PUT -d @- --header "X-PachubeApiKey: YOUR_PACHUBE_API_KEY" http://api.pachube.com/v2/feeds/42055
 done

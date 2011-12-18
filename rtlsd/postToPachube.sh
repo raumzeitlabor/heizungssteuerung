159b7158c9ad4ff8227172cbf20d7d9d4556155d
Severin Schols <severin@schols.de>
2011-12-18 18:33:04 +0100

    easier key management

--- rtlsd/postToPachube.sh
+++ rtlsd/postToPachube.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+PACHUBE_API_KEY="YOUR_API_KEY"
+
+## Initialize serial port
 stty -F /dev/ttyUSB0 cs8 9600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts 
 
 while [ 1 ]
 do
-head -n 1 /dev/ttyUSB0 | xargs -I xxx sed 's/%VALUE%/xxx/g' template.json | curl --request PUT -d @- --header "X-PachubeApiKey: YOUR_PACHUBE_API_KEY" http://api.pachube.com/v2/feeds/42055
+head -n 1 /dev/ttyUSB0 | xargs -I xxx sed 's/%VALUE%/xxx/g' template.json | curl --request PUT -d @- --header "X-PachubeApiKey: $PACHUBE_API_KEY" http://api.pachube.com/v2/feeds/42055
 done

aa46221950bbcc43a74b5efa354da5bef2c0e329
Severin Schols <severin@schols.de>
2013-01-02 15:52:45 +0100

    MQTT all the things

--- software/logger.py
+++ software/logger.py
@@ -22,32 +22,28 @@ import hausbus2
 import config
 
 sensor_values = {}
+running = True
 
 def outputThread():
-	# Do stuff to send results to Pachube
-	feed = eeml.Pachube(config.API_URL, config.API_KEY, use_https=False)
-	while 1 :
-		update_data = []
-		hausbus2.variables["temperature"].clear
+	temperature = {}
+	while running :
+		temperature_old = temperature
+		temperature = {}
 		for key, values in sensor_values.items():
 			value = round(float(sum(values)) / len(values),2)
-			update_data.append(eeml.Data(config.sensors[key]["pachube"], value, unit=eeml.Celsius()))
-			hausbus2.variables["temperature"][config.sensors[key]["hausbus"]] = value
-			tsdb_command = "temperatur "+str(time.strftime("%s"))+" "+str(value)+" sensor="+config.sensors[key]["hausbus"]
-			print tsdb_command
-		feed.update(update_data)
-		try:
-			feed.put()
-		except Exception, err:
-			print >> sys.stderr, "Couldn't send data to pachube: ", err
+			temperature[config.sensors[key]["hausbus"]] = value
+		if temperature != temperature_old:
+			hausbus2.update_group("temperature", temperature, False)
+			hausbus2.publish("temperature", retain = True)
 		sensor_values.clear()
 		time.sleep(30)
 
 def serialThread():
+	global io_ports
 	ser = serial.Serial('/dev/ttyUSB0', 19200)
 	temperatur_regex = re.compile("^sensor: ([0-9a-f]{16}) (-?\d+\.\d+)")
 	port_regex = re.compile("^windows ([abd]): ([01]{8})")
-	while 1:
+	while running:
 		line = ser.readline().strip()
 		match = temperatur_regex.search(line)
 		if (match) :
@@ -56,28 +52,37 @@ def serialThread():
 			sensor_values[match.group(1)].append(float(match.group(2)))
 		match = port_regex.search(line)
 		if (match) :
-			hausbus2.variables["io_ports"][match.group(1)] = match.group(2)
-			updateWindows()
+			io_ports_old = io_ports
+			io_ports[match.group(1)] = match.group(2)
+			if io_ports_old != io_ports:
+				hausbus2.update_group("io_ports", io_ports, False)
+				hausbus2.publish("io_ports", retain = True)
+				updateWindows()
 
 def initWindows():
-	hausbus2.variables["windows"]["state"] = copy.deepcopy(config.windows)
+	windows["state"] = copy.deepcopy(config.windows)
 	for row_id, row in enumerate(config.windows):
 		for col_id, window in enumerate(row):
 			if (window != "x" and window != "?") :
-				hausbus2.variables["windows"]["state"][row_id][col_id] = "?"
+				windows["state"][row_id][col_id] = "?"
 
 def updateWindows():	
+	windows_old = windows
 	for row_id, row in enumerate(config.windows):
 		for col_id, window in enumerate(row):			
 			if (window != "x" and window != "?") :
-				if (window[0].lower() in hausbus2.variables["io_ports"]) :
-					hausbus2.variables["windows"]["state"][row_id][col_id] = hausbus2.variables["io_ports"][window[0].lower()][int(window[1])]
+				if (window[0].lower() in io_ports) :
+					windows["state"][row_id][col_id] = io_ports[window[0].lower()][int(window[1])]
 				else :
-					hausbus2.variables["windows"]["state"][row_id][col_id] = "!"
+					windows["state"][row_id][col_id] = "!"
+	if windows == windows_old:
+		hausbus2.update_group("windows", windows, False)
+		hausbus2.publish("windows", retain = True)
 
-hausbus2.variables["temperature"] = {}
-hausbus2.variables["io_ports"] = {}
-hausbus2.variables["windows"] = {}
+hausbus2.start("heizungssteuerung", http_port=80, mqtt_broker = config.mqtt_broker)
+
+io_ports = {}
+windows = {}
 initWindows()
 
 try:
@@ -86,5 +91,19 @@ try:
 except:
 	print >> sys.stderr, "Error: unable to start threads"
 
+try:
+	while 1:
+		time.sleep(10)
+# Ctrl-C interupts our server magic
+except KeyboardInterrupt:
+	print '^C received, shutting down server'
 
-hausbus2.start(80)	
+running = False
+hausbus2.update_group("temperature", "", False)
+hausbus2.publish("temperature", retain = True)
+hausbus2.update_group("io_ports", "", False)
+hausbus2.publish("io_ports", retain = True)
+hausbus2.update_group("windows", "", False)
+hausbus2.publish("windows", retain = True)
+
+hausbus2.stop()
